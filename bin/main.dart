0903fdf04a63b84d01e6f0df74ec195ac5790d54
muhammadjonyoldoshev <muhammadjonyoldoshev405@gmail.com>
2022-04-05 11:27:45 +0500

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(){
+  /// 15 - masala
+  stdout.write(" S somni kiriting == ");
+  int s = int.parse(stdin.readLineSync()!);
+  stdout.write(" P foizni kiriting == ");
+  int p = int.parse(stdin.readLineSync()!);
+  int result = s;
+  int a = 0;
+  while(result <= (s * 2)){
+    result = p * result ~/ 100;
+    a++;
+  }
+  print("Oy => $a");
+}
